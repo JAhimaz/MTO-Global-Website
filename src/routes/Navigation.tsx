178f2717ed09cc474b3524cd614739347dd42b6b
JoeyN96 <joeynelson91@gmail.com>
2022-07-01 05:09:02 +0800

    adjust navbar links and image posisitoning

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown} from "react-bootstrap";
+import {Navbar, Nav, NavDropdown, Row, Col} from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import Logo from '../assets/logo.png'
 
@@ -7,28 +7,33 @@ import "../css/Navigation.css"
 
 export const Navigation = () => {
     return (
-        <Container>
-            <Navbar className="navbar-bgcolor center-margin" expand="lg">
-                <Container>
-                    <Navbar.Brand className="logo-position" href="#home">
-                        <img src={Logo} className="navigation-logo"/>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
+        <Navbar className="navbar-bgcolor center-margin" expand="lg">            
+            <Container fluid className="no-margin">
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Row className="full-width">                    
+                    <Navbar.Collapse className="full-width">                    
+                        <Nav className="navbar-links full-width">                    
+                            <Col lg={2} xl={2}>
+                                <Nav.Link href="#home">OUR SERVICE</Nav.Link>
+                            </Col>
+                            <Col lg={2} xl={3}>
+                                <Nav.Link href="#link">HOW TO ORDER</Nav.Link>
+                            </Col>
+                            <Col lg={3} xl={2}>
+                                <Navbar.Brand className="logo-position" href="#home">
+                                <img src={Logo} className="navigation-logo" alt="MTO GLobal"/>
+                                </Navbar.Brand>                                
+                            </Col>
+                            <Col lg={2} xl={3}>
+                                <Nav.Link href="#link">ONLINE STORE</Nav.Link>
+                            </Col>
+                            <Col lg={3} xl={3}>
+                                <Nav.Link href="#link">WHERE TO FIND US</Nav.Link>
+                            </Col>                                                
+                        </Nav>
                     </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </Container>
+                </Row>
+            </Container>
+        </Navbar>
     );
 }
