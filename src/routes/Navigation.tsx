26b00310158661208e39fd3b37e56a93db096476
JoeyN96 <joeynelson91@gmail.com>
2022-07-02 03:33:18 +0800

    remove NavDropDown from Navbar

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Navbar, Nav, NavDropdown, Row, Col} from "react-bootstrap";
+import {Navbar, Nav, Row, Col} from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import Logo from '../assets/logo.png'
 
